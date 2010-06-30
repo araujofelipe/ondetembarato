6d554656f22da315baaa73a404ce937ec6c829e8
Felipe Araujo <jfelipe.dev@gmail.com>
2010-06-29 22:09:30 -0600

    Criada estrutura do projeto em Rails3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'b4bb1edf96f754310125aa7dbfefc11df8f87ffe2741d5a8e7eb8303236c3b35f534cb94b5d7818858df6f2042f4b50f180eb35214f8a5f9e0715a8c0121051a'
