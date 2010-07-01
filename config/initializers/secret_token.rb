5ffbcc65291fb11a38428be67c793484c52b4657
Felipe Araujo <jfelipe.dev@gmail.com>
2010-06-30 22:35:55 -0600

    Criando estrutura rails2.x

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = '7677550583880ef8150a46817302d0be4686a58360944d39ac8ed3d58aa6cd14f76642e5ead4b78c4ca364334da21f981ef6e0814d0e554e24a4686a76b4d5ee'
